2a56862d741cb82b7c697141e3e1bb871ebf95fb
Issac Davis <trksprkz@gmail.com>
2023-01-14 19:45:12 -0600

    fixed error of unexpected token on stack.screen

--- App.js
+++ App.js
@@ -2,15 +2,16 @@ import { StatusBar } from 'expo-status-bar';
 import { Text, View } from 'react-native';
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from './src/screens/HomeScreen';
+const Stack = createNativeStackNavigator()
 export default function App() {
   return (
 
     <NavigationContainer>
-    <View>
-      <Text className="text-red-400 align-middle">Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <Stack.Navigator>
+        <Stack.Screen name='Home' component={HomeScreen} />
+      </Stack.Navigator>
     </NavigationContainer>
 
   );
