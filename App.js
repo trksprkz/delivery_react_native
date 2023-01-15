cf64fd926b90f74702b5e872947e0d4cce60e2a1
Issac Davis <trksprkz@gmail.com>
2023-01-14 19:34:38 -0600

    added react native navigation

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
+import React from 'react';
 
 export default function App() {
   return (
     <View>
-      <Text className="text-red-400">Open up App.js to start working on your app!</Text>
+      <Text className="text-red-400 align-middle">Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
